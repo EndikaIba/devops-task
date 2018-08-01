fdc1a2e9e56a685e59f353b4ab0d9d273e8a2c28
Endika <32965153+EndikaIba@users.noreply.github.com>
2018-08-01 13:34:58 +0200

    finalversion

--- metrics/app.py
+++ metrics/app.py
@@ -1,8 +1,75 @@
 # -*- coding: utf-8 -*-
+import os
+from checks import *
+from flask import Flask, abort, request, jsonify, g, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 
-from flask import Flask
 
+# initialization
 app = Flask(__name__)
+#create key for the token encryption
+app.config['SECRET_KEY'] = 'ROI hunter test'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+db = SQLAlchemy(app)
+# Instance of the authentification manager
+auth = HTTPBasicAuth()
+
+class Account(db.Model):
+	#Create table and rows
+    __tablename__ = 'accounts'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), index=True)
+    password_hash = db.Column(db.String(64))
+    active = db.Column(db.Boolean)
+    #Encrypt password
+    def hash_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+#Verify if account: exist, is active, correct credentials
+@auth.verify_password
+def verify_password(username, password):
+    account = Account.query.filter_by(username=username).first()
+    if not account or not account.verify_password(password) or not account.active:
+        return False
+    g.account = account
+    return True
+
+#Create accounts
+@app.route('/createAccount', methods = ['POST'])
+def new_account():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400) # missing arguments
+    if Account.query.filter_by(username = username).first() is not None:
+        abort(400) # existing user
+    account = Account(username = username, active= True)
+    account.hash_password(password)
+    db.session.add(account)
+    db.session.commit()
+    return jsonify({ 'Your account have been created': account.username })
+
+#Deactivate account (not delete them). Till is deleted no new account with this name can be created
+@app.route('/deactivateAccount', methods = ['POST'])
+def delete_account():
+	username = request.json.get('username')
+	password = request.json.get('password')
+	account = Account.query.filter_by(username=username).first()
+	if username is None or password is None:
+		abort(400) # missing arguments
+	if account is None or not account.active or not account.verify_password(password):
+		abort(400) # none existing user or wrong pass
+	account = Account.query.filter_by(username=username).first()
+	account.active = False
+	db.session.commit()
+	return jsonify({ 'Your account have deleted': account.username })
 
 
 @app.route("/")
@@ -16,29 +83,36 @@ def say_hello(name):
 
 
 @app.route("/metrics/cpu")
+@auth.login_required
 def metrics_cpu():
-    pass
+    return cpucheck()
 
 
 @app.route("/metrics/ram")
+@auth.login_required
 def metrics_ram():
-    pass
+    return memorycheck()
 
 
 @app.route("/metrics/disk")
+@auth.login_required
 def metrics_disk():
-    pass
+    return diskcheck()
 
 
 @app.route("/metrics/network")
+@auth.login_required
 def metrics_network():
-    pass
+    return netcheck()
 
 
 @app.route("/metrics/services")
+@auth.login_required
 def metrics_services():
-    pass
+	return servicheck()
 
 
 if __name__ == '__main__':
+    if not os.path.exists('db.sqlite'):
+    	db.create_all()
     app.run(debug=True, host='0.0.0.0')
---
+++ metrics/checks.py
@@ -0,0 +1,26 @@
+import psutil
+import json
+
+
+def cpucheck():
+	cpuusage = json.dumps({'cpu_percent': psutil.cpu_percent()})
+	return cpuusage
+
+def memorycheck():
+	memoryusage = json.dumps({'memory_percent': psutil.virtual_memory().percent,
+							'cache_percent': psutil.virtual_memory().cached / psutil.virtual_memory().total * 100,
+							'swap_percent': psutil.swap_memory().percent})
+	return memoryusage
+
+def diskcheck():
+	diskusage = json.dumps({'disk_percent': psutil.disk_usage('/').percent})
+	return diskusage
+
+def netcheck():
+	netusage = json.dumps({'net_fds': psutil.net_connections()})
+	return netusage
+
+def servicheck():
+	serviceusage = json.dumps({p.pid: p.info for p in psutil.process_iter(attrs=['name', 'username'])})
+	return serviceusage
+	
